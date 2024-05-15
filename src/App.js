d53caa6ed1c95947f737728dbe3b0167ff348342
rhyanrodriguez <rhyanleal9@gmail.com>
2024-05-15 10:08:17 -0300

    Little Changes and Adds

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import Company from "./pages/Company";
+import Contact from "./pages/Contact";
+import NewProject from "./pages/NewProject";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </ul>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/company" element={<Company />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/newproject" element={<NewProject />} />
+      </Routes>
+    </Router>
   );
 }
 
