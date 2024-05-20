416fbc0fb7a3429eaedecbc92d922140b411f729
rhyanrodriguez <rhyanleal9@gmail.com>
2024-05-20 08:39:24 -0300

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,8 @@
 import styles from "./Home.module.css";
 import carinha from "../img/obligations-cuate.svg";
 
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <section className={styles.home_container}>
@@ -8,9 +10,16 @@ function Home() {
         Bem-vindo ao <span className={styles.flow}>FLOW</span>
       </h1>
       <p>Comece a baixar agora mesmo!</p>
-      <a href="/prestados">botaoquevailevaraalgumlugar</a>
-      <p>pao</p>
-      <img src={carinha} alt="carinha" />
+      <ul className={styles.list2}>
+        <li>
+          <Link to="/prestados">Prestados</Link>
+        </li>
+        <li>
+          <Link to="/tomados">Tomados</Link>
+        </li>
+      </ul>
+      <p>oao</p>
+      <img src={carinha} alt="home_image" />
     </section>
   );
 }
