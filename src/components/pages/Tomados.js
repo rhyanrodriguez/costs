89996be619c62a62b532eea123cb96e6bcfff2ca
rhyanrodriguez <rhyanleal9@gmail.com>
2024-05-22 17:19:22 -0300

    Update Tomados

--- src/components/pages/Tomados.js
+++ src/components/pages/Tomados.js
@@ -1,14 +1,24 @@
 import ButtonUI from "../layout/ButtonUI";
 import styles from "./Tomados.module.css";
 import Date from "../layout/Date";
+import Loading from "../layout/Loading";
 
 function Tomados() {
   return (
     <div className={styles.baixar_notas}>
-      <h1>Baixar Notas</h1>
-      <h3>Selecionar Competência:</h3>
-      <Date />
-      <ButtonUI />
+      <div className={styles.section1}>
+        <h1>
+          UneCont <span className="vaitomados">Tomados</span>
+        </h1>
+        <h3>Selecionar Competência:</h3>
+        <Date />
+        <ButtonUI />
+      </div>
+      <div className={styles.section2}>
+        <h1>Baixando Notas</h1>
+        <p>Carregando... aguarde! </p>
+        <Loading />
+      </div>
     </div>
   );
 }
