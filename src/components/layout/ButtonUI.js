9072043ae3e9c39ca210f34fb298ba3f2553281e
rhyanrodriguez <rhyanleal9@gmail.com>
2024-05-22 17:18:47 -0300

    Update Button Propertie 😄

--- src/components/layout/ButtonUI.js
+++ src/components/layout/ButtonUI.js
@@ -1,13 +1,33 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 import styles from "./ButtonUI.module.css";
 
-export default function ContainedButtons() {
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 1,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
+
+export default function InputFileUpload() {
   return (
-    <Stack direction="row" spacing={2}>
-      <Button className={styles.buttonUi} variant="contained">
-        Baixar
-      </Button>
-    </Stack>
+    <Button
+      className={styles.buttonUi}
+      component="label"
+      role={undefined}
+      variant="contained"
+      tabIndex={-1}
+      startIcon={<CloudDownloadIcon />}
+    >
+      Baixar
+      <VisuallyHiddenInput type="file" />
+    </Button>
   );
 }
