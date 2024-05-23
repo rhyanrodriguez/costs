5a0b81056c16fff0d95be2d6cac71dc753c875b2
rhyanrodriguez <rhyanleal9@gmail.com>
2024-05-23 09:15:42 -0300

    Update Tomados

--- src/components/pages/Tomados.js
+++ src/components/pages/Tomados.js
@@ -8,7 +8,7 @@ function Tomados() {
     <div className={styles.baixar_notas}>
       <div className={styles.section1}>
         <h1>
-          UneCont <span className="vaitomados">Tomados</span>
+          UneCont <span className={styles.tomatoma}>Tomados</span>
         </h1>
         <h3>Selecionar Competência:</h3>
         <Date />
@@ -16,7 +16,7 @@ function Tomados() {
       </div>
       <div className={styles.section2}>
         <h1>Baixando Notas</h1>
-        <p>Carregando... aguarde! </p>
+        <p>34 de 122 notas baixadas </p>
         <img
           className={styles.loading_logo}
           src={loadingLogo}
