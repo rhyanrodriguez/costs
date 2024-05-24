97e597970c9889f565ac849e3a07d40ae9d0dd90
rhyanrodriguez <rhyanleal9@gmail.com>
2024-05-24 12:40:20 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
       .then((response) => setData(response.data.message))
       .catch((error) => console.error("Error fetching data: ", error));
   }, []);
+
   return (
     <Router>
       <Navbar />
